f22fc97e31accc920cbe4d1205ac95fd1518036e
michael keung <mikekeung@gmail.com>
2016-07-31 21:39:16 -0400

    generate users views, setup basic layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/home', to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/services', to: 'pages#services'
+
+  root to: 'pages#home'
 end
